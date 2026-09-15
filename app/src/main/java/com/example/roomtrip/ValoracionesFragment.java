76c14ff7c1dda65dc342eedcb8e4248004cc40af
WillRider2003 <a20221862@pucp.edu.pe>
2026-09-15 15:05:49 -0500

    Vistas terminadas, falta conexion

---
+++ app/src/main/java/com/example/roomtrip/ValoracionesFragment.java
@@ -0,0 +1,9 @@
+package com.example.roomtrip;
+
+import androidx.fragment.app.Fragment;
+
+public class ValoracionesFragment extends Fragment {
+    public ValoracionesFragment() {
+        super(R.layout.fragment_valoraciones);
+    }
+}
